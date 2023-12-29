1a1b6e51019068fe2a91474500072b220c067195
s-ko <song.1209@icloud.com>
2023-12-30 00:16:56 +0900

    add: handleSubmit, handleEdit

--- src/App.tsx
+++ src/App.tsx
@@ -16,14 +16,45 @@ function App() {
     setInputValue(event.target.value);
   }
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event?.preventDefault();
+
+    const newTodo: Todo = {
+      inputValue: inputValue,
+      id: todos.length,
+      checked: false,
+    };
+
+    setTodos([newTodo, ...todos]);
+    setInputValue("");
+  };
+
+  const handleEdit = (id: number, inputValue: string) => {
+    const newTodos = todos.map((todo) => {
+      if(todo.id === id) {
+        todo.inputValue = inputValue;
+      }
+      return todo;
+    })
+
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
       <div>
         <h2>Todo</h2>
-        <form onSubmit={() => {}}>
+        <form onSubmit={(event) => handleSubmit(event)}>
           <input type="text" onChange={(event) => handleChange(event)} className='inputText' />
           <input type="submit" value='作成' className='submitButton' />
         </form>
+        <ul className='todoList'>
+          {todos.map(todo => (
+            <li key={todo.id}>
+              <input type="text" onChange={(event) => handleEdit(todo.id, event.target.value)} className='inputText' value={todo.inputValue} />
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
